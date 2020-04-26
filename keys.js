eaaf60766301e47f91c0aa05bcf601f52eb9a56a
KarlaSanchezM <karlamouret1513@gmail.com>
2020-04-26 17:31:54 -0500

    AutenticationNodejs

---
+++ keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'remotemysql.com',
+        user: 's7XkUsxOcq',
+        password: '01dLMXQnYA',
+        database: 's7XkUsxOcq'
+    }
+
+};
